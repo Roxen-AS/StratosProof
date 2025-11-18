ecd408f2b37ccedcfdf4fc72cc36568f5a1b0dbb
Akash Sarangi <87138586+Roxen-AS@users.noreply.github.com>
2025-11-18 20:54:26 +0000

    Update Deploy.s.sol

---
+++ contracts/Deploy.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+import "forge-std/Script.sol";
+import "./StratosProofVerifier.sol";
+import "./IntentEmitter.sol";
+
+
+contract Deploy is Script {
+function run() external {
+vm.startBroadcast();
+
+
+StratosProofVerifier verifier = new StratosProofVerifier();
+console2.log("Verifier deployed at:", address(verifier));
+
+
+IntentEmitter emitter = new IntentEmitter();
+console2.log("Emitter deployed at:", address(emitter));
+
+
+vm.stopBroadcast();
+}
+}
