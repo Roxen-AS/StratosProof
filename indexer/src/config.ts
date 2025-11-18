77ad26627ed8c1674c63325b60bebf9711fd8933
Akash Sarangi <87138586+Roxen-AS@users.noreply.github.com>
2025-11-18 19:09:12 +0000

    Update config.ts

---
+++ indexer/src/config.ts
@@ -0,0 +1,7 @@
+export const CONFIG = {
+RPC: process.env.RPC!,
+VERIFIER: process.env.VERIFIER!,
+EMITTER: process.env.EMITTER!,
+PRIVATE_KEY: process.env.PRIVATE_KEY!,
+IPFS_API: process.env.IPFS_API || "https://ipfs.io/api/v0/add",
+};
