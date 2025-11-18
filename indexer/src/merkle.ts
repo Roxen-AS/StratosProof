11b2cbcfb31ab2d09044e55f2245ebb99d43a127
Akash Sarangi <87138586+Roxen-AS@users.noreply.github.com>
2025-11-18 19:10:00 +0000

    Update merkle.ts

---
+++ indexer/src/merkle.ts
@@ -0,0 +1,11 @@
+import { MerkleTree } from 'merkletreejs';
+import keccak256 from 'keccak256';
+
+
+export function buildMerkle(leaves: Buffer[]) {
+const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+return {
+root: tree.getHexRoot(),
+tree,
+};
+}
