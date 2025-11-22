c366e33535938990b76bd84019c09972371663d1
Akash Sarangi <87138586+Roxen-AS@users.noreply.github.com>
2025-11-22 17:37:51 +0000

    UPDATES

--- indexer/src/config.ts
+++ indexer/src/config.ts
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 export const CONFIG = {
   RPC: process.env.RPC!,
+  PRIVATE_KEY: process.env.PRIVATE_KEY!,
   VERIFIER: process.env.VERIFIER!,
   EMITTER: process.env.EMITTER!,
-  PRIVATE_KEY: process.env.PRIVATE_KEY!,
-  IPFS_API: process.env.IPFS_API || "https://ipfs.io/api/v0/add"
+  IPFS_API: process.env.IPFS_API!,
 };
