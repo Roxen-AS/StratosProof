0ec99ba1823063b4627fe0852762965bbc0ee631
Akash Sarangi <87138586+Roxen-AS@users.noreply.github.com>
2025-11-23 05:44:14 +0000

    Prepare frontend for Vercel

--- contracts/StratosProofVerifier.sol
+++ contracts/StratosProofVerifier.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.20;
 
 contract StratosProofVerifier {
     address public admin;
-    mapping(bytes32 => bool) public anchoredRoots;
+    mapping(bytes32 => bool) public roots;
 
-    event RootAnchored(bytes32 indexed root, address indexed by, uint256 timestamp);
+    event RootAnchored(bytes32 indexed root, uint256 timestamp);
 
     constructor() {
         admin = msg.sender;
@@ -17,31 +17,11 @@ contract StratosProofVerifier {
     }
 
     function registerRoot(bytes32 root) external onlyAdmin {
-        anchoredRoots[root] = true;
-        emit RootAnchored(root, msg.sender, block.timestamp);
+        roots[root] = true;
+        emit RootAnchored(root, block.timestamp);
     }
 
     function isAnchored(bytes32 root) external view returns (bool) {
-        return anchoredRoots[root];
-    }
-
-    function verifyProof(
-        bytes32 leaf,
-        bytes32[] calldata siblings,
-        uint256 index,
-        bytes32 root
-    ) public pure returns (bool) {
-        bytes32 hash = leaf;
-
-        for (uint256 i = 0; i < siblings.length; i++) {
-            if (index % 2 == 1) {
-                hash = keccak256(abi.encodePacked(siblings[i], hash));
-            } else {
-                hash = keccak256(abi.encodePacked(hash, siblings[i]));
-            }
-            index = index / 2;
-        }
-
-        return hash == root;
+        return roots[root];
     }
 }
